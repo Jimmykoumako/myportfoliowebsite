c15c40df152225c9efc4febd928c66e09342870c
Gedeon Koumako <jimmykoumako@yahoo.com>
2024-02-01 18:09:12 -0500

    Update my infos

--- js/script.js
+++ js/script.js
@@ -1,38 +1,41 @@
-$(function(){
+$(function() {
+    var animationDuration = 900;
 
-    $(".navbar a, footer a").on("click", function(event){
+    // Smooth scrolling for navigation
+    $(".navbar a, footer a").on("click", function(event) {
         event.preventDefault();
         var hash = this.hash;
-        $('body,html').animate({scrollTop: $(hash).offset().top} , 900 , function(){window.location.hash = hash;})
+        $('body,html').animate({scrollTop: $(hash).offset().top}, animationDuration, function() {
+            window.location.hash = hash;
+        });
     });
 
-
-
-    $('#contact-form').submit(function(e){
+    // Form submission via AJAX
+    $('#contact-form').submit(function(e) {
         e.preventDefault();
         $('.comments').empty();
         var postdata = $('#contact-form').serialize();
 
         $.ajax({
             type: 'POST',
-            url: 'php/contact.php',
+            url: './php/contact.php',
             data: postdata,
             dataType: 'json',
-            success: function(result){
-                if(result.isSuccess){
+            success: function(result) {
+                if (result.isSuccess) {
                     $("#contact-form").append("<p class='thank-you'>Votre message a bien été envoyé. Merci de m'avoir contacté !</p>");
                     $("#contact-form")[0].reset();
-                }
-                else{
+                } else {
                     $("#firstname + .comments").html(result.firstnameError);
                     $("#name + .comments").html(result.nameError);
                     $("#email + .comments").html(result.emailError);
                     $("#phone + .comments").html(result.phoneError);
                     $("#message + .comments").html(result.messageError);
                 }
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.error("AJAX Error:", errorThrown);
             }
         });
-
-
     });
-})
+});
